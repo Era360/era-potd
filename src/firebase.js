cd4531d809e02cbbae70c5c9fa100c1db040bbf9
Era360 <mkumboelia@gmail.com>
2022-01-24 22:55:19 +0300

    initialized analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeAnalytics(app);
+// const analytics = getAnalytics(app);
 // logEvent(analytics, "notification_received");
